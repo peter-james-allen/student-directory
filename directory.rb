1bfbc95531567c29a9938972fa91302effa8bbf2
Pete <peter_james_allen@hotmail.com>
2021-01-22 12:22:23 +0000

    created user input method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,33 @@
-#list of students in array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+# list of students in array
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Krueger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Joffrey Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+# ]
+# define student inuput method
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  name = gets.chomp
+  # while name isn't empty, repeat
+  while !name.empty? do
+    #add student to array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 # define print methods
 def print_header
   puts "The students of Villains Academy"
@@ -25,7 +41,8 @@ end
 def print_footer(names)
   print "Overall, we have #{names.count} great students"
 end
-#call methods
+# call methods
+students = input_students
 print_header
 print_students(students)
 print_footer(students)
