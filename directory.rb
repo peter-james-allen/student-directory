93a920cb36b65628a4af9a5e23dcd9d591416a34
Peter Allen <peter_james_allen@hotmail.com>
2021-01-28 22:44:01 +0000

    added file load method

--- directory.rb
+++ directory.rb
@@ -104,6 +104,7 @@ def print_menu
   puts "1. Input Students"
   puts "2. Show Students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -116,6 +117,15 @@ def show_students
   else
     puts "No students are enrolled"
   end
+end
+
+def load_students
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+    name,cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def save_students
@@ -137,6 +147,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
